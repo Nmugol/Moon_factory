94e33f20f1bc5a4fb51228ffb59a7ab5e03abffd
Norbert Muzolf <nmugol000@gmail.com>
2023-07-31 14:55:51 +0200

    Fix texur

--- Script/teren_gen.gd
+++ Script/teren_gen.gd
@@ -1,11 +1,22 @@
 extends TileMap
 
-@export var map_size:int = 100
+@export var chank_size:int = 16
+@export var world_size:int = 9
 
-# Called when the node enters the scene tree for the first time.
+var tile_x_poz = FastNoiseLite.new()
+
 func _ready():
-	for i in map_size:
-		for j in map_size:
-			set_cell(0,Vector2i(i,j),0,Vector2i(0,0),0)
-	pass # Replace with function body.
-
+	tile_x_poz.seed = randi()
+	var map_size:int = chank_size*world_size
+	var start_poz:int = round(map_size/2)*(-1)
+	for poz_x in range(start_poz,map_size):
+		for  poz_y in range(start_poz,map_size):
+			var x = round(tile_x_poz.get_noise_2d(poz_x,poz_y))
+			var tile_x = x+1
+			set_cell(0,Vector2i(poz_x,poz_y),0,Vector2i(tile_x,0),0)
+			pass
+		pass
+	pass
+	
+func  _process(delta):
+	pass
