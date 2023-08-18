74bd7b77ef8d35c728782960bb19da6202bc3e7d
Norbert Muzolf <nmugol000@gmail.com>
2023-08-18 12:28:32 +0200

    add alien spawn on corect biom

--- Script/teren_gen.gd
+++ Script/teren_gen.gd
@@ -1,14 +1,14 @@
 extends TileMap
 
-@export var chunk_size:int = 16 # Rozmiar pojedynczego kafelka w pikselach
+@export var chunk_size:int = 16 # Rozmiar chanku
 @export var world_size:int = 9  # Rozmiar świata, liczba chunków na jednej osi
 
 ## Ustawienie rozmiaru mapy i pozycji pierwszego kafelka
 var map_size:int = chunk_size * world_size
-var start_pos:int = round(map_size / 2) * (-1)
+var start_pos:int = round(map_size / 2.0) * (-1)
 	
 ## Utworzenie tablicy dwuwymiarowej o wymiarach start_pos*(-1) na start_pos*(-1)
-var map_array = Empty2DArray(start_pos * (-1), start_pos * (-1))
+var map_array = Empty2DArray(map_size)
 
 var tile_x_noise = FastNoiseLite.new()     # Obiekt FastNoiseLite dla pozycji X kafelków
 var tile_y_noise = FastNoiseLite.new()     # Obiekt FastNoiseLite dla pozycji Y kafelków
@@ -23,10 +23,6 @@ class SingleTile:
 	var type_y:int
 
 func _ready():
-	GenerateTerrain() # Funkcja generująca teren przy uruchomieniu gry
-	pass
-	
-func GenerateTerrain():
 	## Ustawienie szumu dla pozycji X i Y kafelków
 	tile_x_noise.seed = randi()
 	tile_y_noise.seed = randi()
@@ -38,10 +34,16 @@ func GenerateTerrain():
 	tile_layer_noise.seed = randi()
 	tile_layer_noise.fractal_octaves = 1
 	
+	GenerateTerrain() # Funkcja generująca teren przy uruchomieniu gry
+	GenrateNest()# Funkcja ustawiające niazda opcych
+	
+	pass
+
+func GenerateTerrain():
 	# Pętle generujące kafelki
 	for pos_x in range(start_pos, start_pos * (-1)):
 		for pos_y in range(start_pos, start_pos * (-1)):
-			var layer = round(tile_layer_noise.get_noise_2d(pos_x, pos_y) * 2.5 + 2.5)  # Wygenerowanie wartości dla warstwy
+			var layer = round(tile_layer_noise.get_noise_2d(pos_x, pos_y) * 3 + 3)  # Wygenerowanie wartości dla warstwy
 			var tile_x = round(tile_x_noise.get_noise_2d(pos_x, pos_y) * 3 + 2)         # Wygenerowanie wartości dla pozycji X kafelka
 			var tile_y = round(tile_y_noise.get_noise_2d(pos_x, pos_y) * 2 + 1)         # Wygenerowanie wartości dla pozycji Y kafelka
 			
@@ -66,12 +68,45 @@ func GenerateTerrain():
 	tile_x_noise = null
 	tile_y_noise = null
 	tile_layer_noise = null
+
+func GenrateNest():
+	var owset = 4
 	
-func Empty2DArray(n: int, m: int) -> Array:
+	#odczyt mapy
+	for i in range(map_array.size()-owset):
+		for j in range(map_array.size()-owset):
+			
+			#pobranie kafelak
+			var cell = map_array[i][j]
+			
+			#sprawdzeniae biomu
+			if cell.type_x == 0:
+				
+				#losowanie szansy na postawienie
+				var chance = randi()%100+1
+				if chance <= 5:
+					
+					#zmienna pomocnicza do sprawdzanie czy można położyć gniazdo
+					var can_pleas_nest:bool = true
+					
+					#sprawdzanie czy teren jest odpowieni 
+					for k in range(1,owset):
+						for l in range(1,owset):
+							#jesl min jeden kafelek nie spełna warunków przerywamy pętlę
+							if map_array[i+k][j+l].type_x != 0 and cell.layer != map_array[i+k][j+l].layer:
+								can_pleas_nest = false
+								break
+						if can_pleas_nest == false:
+							break
+					# Usatwiamy kafrelet na odpowieniej pozycji 
+					if can_pleas_nest == true:
+						set_cell(cell.layer+6,Vector2i(cell.x,cell.y),1,Vector2i(0,0),0)
+
+func Empty2DArray(n: int) -> Array:
 	var result = []
 	for i in range(n):
 		var row = []
-		for j in range(m):
+		for j in range(n):
 			row.append(0)
 		result.append(row)
 	return result
